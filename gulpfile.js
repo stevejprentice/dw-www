8e61e7d36db895ff234f5bd4cf2218b574133136
David Wesst <david@wes.st>
2016-02-19 19:49:34 +0000

    building styles with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,16 @@
 var gulp        = require('gulp'),
     connect     = require('gulp-connect'),
     ghPages     = require('gulp-gh-pages'),
-    minifyHTML  = require('gulp-minify-html');
+    minifyHTML  = require('gulp-minify-html'),
+    rename      = require('gulp-rename'),
+    sass        = require('gulp-sass');
+    
+var path        = require('path');
     
 var DIST_WEB    = './dist';
 
-gulp.task('default', ['build-web', 'run', 'watch']);
-gulp.task('build', ['build-html', 'build-config']);
+gulp.task('default', ['build', 'run', 'watch']);
+gulp.task('build', ['build-html', 'build-style', 'build-config']);
 
 gulp.task('build-config', function() {
    return gulp.src('./web.config')
@@ -18,6 +22,13 @@ gulp.task('build-html', function() {
                 .pipe(minifyHTML())
                 .pipe(gulp.dest(DIST_WEB))
                 .pipe(connect.reload());
+});
+
+gulp.task('build-style', ['process-normalize'], function() {
+    return gulp.src('./src/**/*.scss')
+            .pipe(sass().on('error', sass.logError))
+            .pipe(gulp.dest(DIST_WEB))
+            .pipe(connect.reload());
 });
 
 gulp.task('deploy', function() {
@@ -40,5 +51,13 @@ gulp.task('run', function() {
 gulp.task('watch', function() {
     //gulp.watch('./src/**/*.ts', ['build-src']);
     gulp.watch('./src/*.html', ['build-html']);
-    //gulp.watch('./src/style/*.scss', ['build-style']);
+    gulp.watch('./src/style/*.scss', ['build-style']);
+});
+
+gulp.task('process-normalize', function() {
+    var libSrc = './node_modules/normalize.css/normalize.css';
+    
+    return gulp.src(libSrc)
+                .pipe(rename('_normalize.scss'))
+                .pipe(gulp.dest(path.join('./src/style/')));
 });
