ae7df6f5068a9ee634d1cc408bdc974dbd4694e2
David Wesst <david.wesst@umanitoba.ca>
2016-07-21 15:06:58 -0500

    displaying links to blog posts

--- src/script/main.js
+++ src/script/main.js
@@ -2,28 +2,62 @@
     'use strict';
     window.DW = { };
 
-    DW.postData = '';
+    DW.rawData = '';
+    DW.gameDevPosts = [];
+    DW.techPosts = [];
 
     DW.displayPosts = 
         ()=> {
-            alert('displaying post...');
+            $('#techPosts, #gamedevPosts').empty();
+
+            for(let i = 0; i < 5 && i <= DW.techPosts.length; i++) {
+                let currPost = DW.techPosts[i];
+                $('#techPosts').append('<li><a href="' + currPost.permalink + '">' + currPost.title + '</a></li>');
+            }
+
+            for(let i = 0; i < 5 && i <= DW.techPosts.length; i++) {
+                let currPost = DW.gameDevPosts[i];
+                $('#gamedevPosts').append('<li>' + currPost.title + '</li>');
+            }
+        }
+
+    DW.storePosts = 
+        (posts)=> {
+            DW.rawData = posts;
+
+            DW.techPosts 
+                = _.filter(posts, (post)=> {
+                    return (post.categories && post.categories.length > 0 ? post.categories[0].name === 'development' : false);
+                }); 
+
+            DW.gameDevPosts 
+                = _.filter(posts, (post)=> {
+                    return (post.categories && post.categories.length > 0 ? post.categories[0].name === 'gamedev' : false);
+                }); 
         }
 
     DW.getPosts = 
         (postType)=> {
             // if no post data
-            if(DW.postData === '') {
+            if(DW.rawData === '') {
                 // get post and store it
                 $.getJSON('https://raw.githubusercontent.com/davidwesst/dw-blog/gh-pages/content.json')
                     .done((data)=> {
-                        DW.postData = data;
+                        DW.storePosts(data.posts);
                     })
                     .fail(()=> {
-                        DW.postData = 'error';
+                        DW.rawData = 'error';
                     })
                     .always(()=> {
                         DW.displayPosts();
                     })
             }
         }
+
+    // initialization code
+    $(document).scroll(()=> {
+        if(DW.rawData === '') {
+            DW.getPosts();
+        }
+    });
 })();
