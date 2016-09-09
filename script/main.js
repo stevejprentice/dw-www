5861ebd0ed01d276788150dd9ee678ae535cdf3a
DW via Travis CI <questions@davidwesst.com>
2016-09-09 18:45:46 +0000

    Update 2016-09-09T18:45:45.662Z

--- script/main.js
+++ script/main.js
@@ -3,16 +3,17 @@
     window.DW = { };
 
     DW.posts = '';
+    DW.talks = '';
 
     DW.displayPosts = 
-        (postType)=> {
+        function(postType) {
             
             function showPosts(listSelector, posts) {
                 $(listSelector).empty();
 
-                for(let i = 0; i < 5 && i <= posts.length; i++) {
-                    let currPost = posts[i];
-                    let content = [];
+                for(var i = 0; i < 5 && i < posts.length; i++) {
+                    var currPost = posts[i];
+                    var content = [];
                     content.push('<li>');
                     content.push('<a class="post" href="' + currPost.permalink + '">');
 
@@ -58,7 +59,7 @@
                 $(listSelector).append('<li>' + 'There are currently no posts of this type.<br /><br /> Checkout <a href="https://blog.davidwesst.com">DW\'s blog</a> for other posts.' + '</li>');
             }
 
-            let techPostsSelector = '#techPosts';
+            var techPostsSelector = '#techPosts';
             if(DW.posts.length > 0) {
                 showPosts(techPostsSelector, DW.posts);
             }
@@ -67,8 +68,59 @@
             }
         }
 
+    DW.displayTalks = 
+        function() {
+            
+            function showTalks(listSelector, talks) {
+                $(listSelector).empty();
+
+                for(var i = 0; i < 5 && i < talks.length; i++) {
+                    var currTalk = talks[i];
+                    var content = [];
+                    content.push('<li>');
+                    content.push('<a class="post" href="' + currTalk.eventUrl + '">');
+
+                    content.push('<article class="sidebar">');
+                    content.push('<div>' + moment(currTalk.date).format('YYYY') + '</div>');
+                    content.push('<div>' + moment(currTalk.date).format('MMM') + '</div>');
+                    content.push('<div>' + moment(currTalk.date).format('DD') + '</div>');
+                    content.push('</article>');
+
+                    content.push('<article class="content">');
+
+                    content.push('<h4>');
+                    content.push(currTalk.title);
+                    content.push('</h4>');
+
+                    content.push('<h6 class="subtitle">');
+                    content.push(currTalk.event);
+                    content.push('</h6>');
+
+                    content.push('</article>');
+                    content.push('</a>');
+                    content.push('</li>');
+
+                    $(listSelector).append(content.join(''));
+                }
+            }
+
+            function displayNoTalkMessage(listSelector) {
+                $(listSelector).empty();
+
+                $(listSelector).append('<li>' + 'There are currently no talks available.' + '</li>');
+            }
+
+            var techPostsSelector = '#talkList';
+            if(DW.talks.length > 0) {
+                showTalks(techPostsSelector, DW.talks);
+            }
+            else {
+                displayNoTalkMessage(techPostsSelector);
+            }
+        }
+
     DW.storePosts = 
-        (posts)=> {
+        function(posts) {
             DW.posts = [];
 
             posts.forEach(function(post) {
@@ -79,30 +131,66 @@
                 DW.posts.push(post);
             }, this);
         }
+    
+    DW.storeTalks = 
+        function(talks) {
+            DW.talks = [];
+
+            talks.forEach(function(talk) {
+                if(talk.excerpt && talk.excerpt.length > 250) {
+                    talk.excerpt = talk.excerpt.slice(0,250) + '...';
+                }
+                
+                DW.talks.push(talk);
+            }, this);
+        }
 
     DW.getPosts = 
-        (postType)=> {
+        function(postType) {
             // if no post data
-            if(DW.posts === '') {
+            if(DW.talks === '') {
                 // get post and store it
                 $.getJSON('https://raw.githubusercontent.com/davidwesst/dw-blog/gh-pages/content.json')
-                    .done((data)=> {
+                    .done(function(data) {
                         DW.storePosts(data.posts);
                     })
-                    .fail(()=> {
+                    .fail(function() {
                         DW.posts = 'error';
                         console.error('An error occured while retrieving blog posts');
                     })
-                    .always(()=> {
+                    .always(function() {
                         DW.displayPosts();
                     })
             }
         }
 
+    DW.getTalks =
+        function() {
+            // if no post data
+            if(DW.talks === '') {
+                // get post and store it
+                $.getJSON('https://raw.githubusercontent.com/davidwesst/dw-talks/master/_data/talks.json')
+                    .done(function(data) {
+                        DW.storeTalks(data.talks);
+                    })
+                    .fail(function() {
+                        DW.talks = 'error';
+                        console.error('An error occured while retrieving talks');
+                    })
+                    .always(function() {
+                        DW.displayTalks();
+                    })
+            }
+        }
+
     // initialization code
-    $(document).ready(()=> {
+    $(document).ready(function() {
         if(DW.posts === '') {
             DW.getPosts();
+        }
+
+        if(DW.talks === '') {
+            DW.getTalks();
         }
     });
 })();
